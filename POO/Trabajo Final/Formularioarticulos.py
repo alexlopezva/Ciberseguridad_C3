0dcc9c235c802bee9910035eda005b37f619e4f0
alexlopezva <lopez0629@gmail.com>
2025-12-05 20:11:49 -0400

    Add files via upload

--- POO/Trabajo Final/Formularioarticulos.py
+++ POO/Trabajo Final/Formularioarticulos.py
@@ -13,6 +13,11 @@ class FormularioArticulos:
         self.carga_articulos()
         self.consulta_por_codigo()
         self.listado_completo()
+<<<<<<< HEAD
+        self.borrado()
+        self.modificar()
+=======
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
         self.cuaderno1.grid(column=0, row=0, padx=10, pady=10)
         self.ventana1.mainloop()
 
@@ -34,6 +39,16 @@ class FormularioArticulos:
         self.boton1=ttk.Button(self.labelframe1, text="Confirmar", command=self.agregar)
         self.boton1.grid(column=1, row=2, padx=4, pady=4)
 
+<<<<<<< HEAD
+    def agregar(self):
+        datos=(self.descripcioncarga.get(), self.preciocarga.get())
+        self.articulo1.alta(datos)
+        mb.showinfo("Información", "Los datos fueron cargados")
+        self.descripcioncarga.set("")
+        self.preciocarga.set("")
+
+=======
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
     def consulta_por_codigo(self):
         self.pagina2 = ttk.Frame(self.cuaderno1)
         self.cuaderno1.add(self.pagina2, text="Consulta por código")
@@ -54,7 +69,11 @@ class FormularioArticulos:
         self.precio=tk.StringVar()
         self.entryprecio=ttk.Entry(self.labelframe2, textvariable=self.precio, state="readonly")
         self.entryprecio.grid(column=1, row=2, padx=4, pady=4)
+<<<<<<< HEAD
+        self.boton1=ttk.Button(self.labelframe2, text="Consultar", command=self.consultar)
+=======
         self.boton1=ttk.Button(self.labelframe2, text="Consultar")
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
         self.boton1.grid(column=1, row=3, padx=4, pady=4)
 
     def consultar(self):
@@ -73,7 +92,11 @@ class FormularioArticulos:
         self.cuaderno1.add(self.pagina3, text="Listado completo")
         self.labelframe3=ttk.LabelFrame(self.pagina3, text="Artículo")
         self.labelframe3.grid(column=0, row=0, padx=5, pady=10)
+<<<<<<< HEAD
+        self.boton1=ttk.Button(self.labelframe3, text="Listado completo", command=self.listar)
+=======
         self.boton1=ttk.Button(self.labelframe3, text="Listado completo")
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
         self.boton1.grid(column=0, row=0, padx=4, pady=4)
         self.scrolledtext1=st.ScrolledText(self.labelframe3, width=30, height=10)
         self.scrolledtext1.grid(column=0,row=1, padx=10, pady=10)
@@ -82,6 +105,79 @@ class FormularioArticulos:
         respuesta=self.articulo1.recuperar_todos()
         self.scrolledtext1.delete("1.0", tk.END)        
         for fila in respuesta:
+<<<<<<< HEAD
+            self.scrolledtext1.insert(tk.END, "código:"+str(fila[0])+
+                                              "\ndescripción:"+fila[1]+
+                                              "\nprecio:"+str(fila[2])+"\n\n")
+
+    def borrado(self):
+        self.pagina4 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina4, text="Borrado de artículos")
+        self.labelframe4=ttk.LabelFrame(self.pagina4, text="Artículo")        
+        self.labelframe4.grid(column=0, row=0, padx=5, pady=10)
+        self.label1=ttk.Label(self.labelframe4, text="Código:")
+        self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.codigoborra=tk.StringVar()
+        self.entryborra=ttk.Entry(self.labelframe4, textvariable=self.codigoborra)
+        self.entryborra.grid(column=1, row=0, padx=4, pady=4)
+        self.boton1=ttk.Button(self.labelframe4, text="Borrar", command=self.borrar)
+        self.boton1.grid(column=1, row=1, padx=4, pady=4)
+
+    def borrar(self):
+        datos=(self.codigoborra.get(), )
+        cantidad=self.articulo1.baja(datos)
+        if cantidad==1:
+            mb.showinfo("Información", "Se borró el artículo con dicho código")
+        else:
+            mb.showinfo("Información", "No existe un artículo con dicho código")
+
+    def modificar(self):
+        self.pagina5 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina5, text="Modificar artículo")
+        self.labelframe5=ttk.LabelFrame(self.pagina5, text="Artículo")
+        self.labelframe5.grid(column=0, row=0, padx=5, pady=10)
+        self.label1=ttk.Label(self.labelframe5, text="Código:")
+        self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.codigomod=tk.StringVar()
+        self.entrycodigo=ttk.Entry(self.labelframe5, textvariable=self.codigomod)
+        self.entrycodigo.grid(column=1, row=0, padx=4, pady=4)
+        self.label2=ttk.Label(self.labelframe5, text="Descripción:")        
+        self.label2.grid(column=0, row=1, padx=4, pady=4)
+        self.descripcionmod=tk.StringVar()
+        self.entrydescripcion=ttk.Entry(self.labelframe5, textvariable=self.descripcionmod)
+        self.entrydescripcion.grid(column=1, row=1, padx=4, pady=4)
+        self.label3=ttk.Label(self.labelframe5, text="Precio:")        
+        self.label3.grid(column=0, row=2, padx=4, pady=4)
+        self.preciomod=tk.StringVar()
+        self.entryprecio=ttk.Entry(self.labelframe5, textvariable=self.preciomod)
+        self.entryprecio.grid(column=1, row=2, padx=4, pady=4)
+        self.boton1=ttk.Button(self.labelframe5, text="Consultar", command=self.consultar_mod)
+        self.boton1.grid(column=1, row=3, padx=4, pady=4)
+        self.boton2=ttk.Button(self.labelframe5, text="Modificar", command=self.modifica)
+        self.boton2.grid(column=1, row=4, padx=4, pady=4)
+
+    def modifica(self):
+        datos=(self.descripcionmod.get(), self.preciomod.get(), self.codigomod.get())
+        cantidad=self.articulo1.modificacion(datos)
+        if cantidad==1:
+            mb.showinfo("Información", "Se modificó el artículo")
+        else:
+            mb.showinfo("Información", "No existe un artículo con dicho código")
+
+    def consultar_mod(self):
+        datos=(self.codigomod.get(), )
+        respuesta=self.articulo1.consulta(datos)
+        if len(respuesta)>0:
+            self.descripcionmod.set(respuesta[0][0])
+            self.preciomod.set(respuesta[0][1])
+        else:
+            self.descripcionmod.set('')
+            self.preciomod.set('')
+            mb.showinfo("Información", "No existe un artículo con dicho código")
+
+
+aplicacion1=FormularioArticulos()
+=======
             self.scrolledtext1.insert(tk.END, "código:"+str(fila[0])+"\ndescripción:"+fila[1]+"\nprecio:"+str(fila[2])+"\n\n")
 
 
@@ -93,4 +189,5 @@ class FormularioArticulos:
         self.precio.set("")
         
 formularioarticulos=FormularioArticulos()
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
 
--- POO/Trabajo Final/articulos.py
+++ POO/Trabajo Final/articulos.py
@@ -5,8 +5,13 @@ class Articulos:
     def abrir(self):
         conexion=mysql.connector.connect(host="localhost", 
                                               user="root", 
+<<<<<<< HEAD
+                                              passwd="Motocross0629*", 
+                                              database="db1")
+=======
                                               passwd="", 
                                               database="bd1")
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
         return conexion
 
 
@@ -32,4 +37,26 @@ class Articulos:
         sql="select codigo, descripcion, precio from articulos"
         cursor.execute(sql)
         cone.close()
+<<<<<<< HEAD
         return cursor.fetchall()
+
+    def baja(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="delete from articulos where codigo=%s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas borradas
+
+    def modificacion(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="update articulos set descripcion=%s, precio=%s where codigo=%s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas modificadas
+=======
+        return cursor.fetchall()
+>>>>>>> cf18b7d6cb668325fedf66730db995aaeaf4d55f
